cc7630a9efa765a0e5116e3db353b2c9a683309b
genivaldo2310 <genivaldo.campos17@gmail.com>
2022-03-29 11:54:06 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/projetoestudo/curso/services/CategoriaServices.java
+++ src/main/java/com/projetoestudo/curso/services/CategoriaServices.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.projetoestudo.curso.domain.Categoria;
 import com.projetoestudo.curso.repositories.CategoriaRepository;
+import com.projetoestudo.curso.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaServices {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
