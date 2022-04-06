4644bca05f400b44c65a19ed613077601de08a86
genivaldo2310 <genivaldo.campos17@gmail.com>
2022-04-06 20:09:11 -0300

    listando todas as categorias com DTO.

--- src/main/java/com/projetoestudo/curso/resources/CategoriaResource.java
+++ src/main/java/com/projetoestudo/curso/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.projetoestudo.curso.resources;
 
 import java.net.URI;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetoestudo.curso.domain.Categoria;
+import com.projetoestudo.curso.dto.CategoriaDTO;
 import com.projetoestudo.curso.services.CategoriaServices;
 
 @RestController
@@ -26,28 +29,35 @@ public class CategoriaResource {
 		Categoria obj = services.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = services.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = services.update(obj);
-		
+
 		return ResponseEntity.noContent().build();
-		
+
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		services.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> categorias = services.findAll();
+		List<CategoriaDTO> categoriasDTO = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriasDTO);
 	}
 }
